26c35e4c3d03ba4d8124ee5da4868e86a10c496e
zhaoxuyan <526161421@qq.com>
2017-08-09 22:56:01 +0800

    :bug: fix days in month/fix leapyear

--- cacu_day_age.py
+++ cacu_day_age.py
@@ -1,7 +1,6 @@
 # coding=utf-8
-### Dave's code ############################################################################
 def nextDay(year, month, day):
-    if day < 30:
+    if day < daysInMonth(year, month):
         return year, month, day + 1
     else:
         if month < 12:
@@ -21,8 +20,6 @@ def dateIsBefore(year1, month1, day1, year2, month2, day2):
     return False
 
 
-### Dave's code ############################################################################
-
 def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     # assert + expression if expression is false : throw an AssertionError
     # assert 后跟表达式, 若表达式为False，中断程序运行
@@ -34,6 +31,25 @@ def daysBetweenDates(year1, month1, day1, year2, month2, day2):
         year1, month1, day1 = nextDay(year1, month1, day1)
         num = num + 1
     return num
+
+
+def isLeapYear(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+
+def daysInMonth(year, month):
+    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+        return 31
+    elif month == 2:
+        if isLeapYear(year):
+            return 29
+        else:
+            return 28
+    elif month == 4 or month == 6 or month == 9 or month == 11:
+        return 30
 
 
 print daysBetweenDates(2013, 1, 1, 2014, 1, 1)
